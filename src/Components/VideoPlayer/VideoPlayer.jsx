b06c4a3afcc237c918111858ff5fb7fbb2fda844
Audrey Schmidt <audsch@outlook.com>
2022-01-19 15:50:02 -0600

    made description in VideoPlayer only show 100characters unless button clicked to show full description with nested conditional rendering

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 const VideoPlayer = (props) => {
-    
-    const [videoURL, setVideoURL] = useState('')
+    const [videoURL, setVideoURL] = useState('');
+    const [showFullDesc, setShowFullDesc] = useState(false);
+
+    const handleDesc = () => setShowFullDesc(!showFullDesc);
+
     useEffect(() => {
         let url = `https://www.youtube.com/embed/${props.video.id.videoId}`;
         setVideoURL(url)
@@ -13,13 +16,19 @@ const VideoPlayer = (props) => {
             <iframe id="ytplayer" type="text/html" width="640" height="360"
             src={videoURL} frameBorder="0"></iframe>
             {'snippet' in props.video 
-                ? <h3>{props.video.snippet.title}</h3>
+                ?  <h3>{props.video.snippet.title}</h3>
                 : <h3>NO SNIPPED AVAILABLE</h3>
                 }
-            {'snippet' in props.video 
+            {showFullDesc
+            ? ('snippet' in props.video
                 ? <p>{props.video.snippet.description}</p>
-                : <p>NO SNIPPED AVAILABLE</p>
+                : <p>NO SNIPPET AVAILABLE</p>)
+            : ('snippet' in props.video 
+                ? <p>{props.video.snippet.description.substring(0,100)}</p>
+                : <p>NO SNIPPET AVAILABLE</p>
+            )
                 }
+                <button onClick={handleDesc}>show description</button>
         </div>
     );
    
