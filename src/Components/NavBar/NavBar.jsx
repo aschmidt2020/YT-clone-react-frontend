99ee6bb9ddfe6a620d9c7a66ea33d7a96ed79c3e
Audrey Schmidt <audsch@outlook.com>
2022-01-20 11:58:56 -0600

    moved nav bar into offcanvas, commented out drop-down menu in case it is needed

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 const NavBar = (props) => {
     return (
         <div>
-            {/* <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+            <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
             Link with href
             </a>
 
             <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
             <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Navigation Bar</h5>
                 <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
-            <div class="offcanvas-body"> */}
+            <div class="offcanvas-body">
                 <nav>
                 <ul>
                     <Link to='/'><li>Home/Search</li></Link>
@@ -29,9 +29,9 @@ const NavBar = (props) => {
                     <li><a class="dropdown-item" href="#">Another action</a></li>
                     <li><a class="dropdown-item" href="#">Something else here</a></li>
                 </ul>
-                </div>
+                </div> */}
             </div>
-            </div> */}
+            </div>
         </div>
     );
 }
