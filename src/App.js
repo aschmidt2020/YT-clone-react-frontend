7d56eb1dc6b40a9db8ca072609dfb6ae93f17ee5
TPavZ <tmpavlic@gmail.com>
2022-01-20 11:40:37 -0600

    Added LoginForm component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import RelatedVideos from './Components/RelatedVideos/RelatedVideos';
 import SearchBar from "./Components/SearchBar/SearchBar";
 import SearchResults from './Components/SearchResults/SearchResults';
 import VideoPlayer from "./Components/VideoPlayer/VideoPlayer";
+import LoginForm from './Components/LoginForm/LoginForm';
 
 function App() {
   const API_KEY = process.env.REACT_APP_API_KEY_YT
@@ -23,54 +24,54 @@ function App() {
     getPlaylist(response.data.items[0]);
   }
 
-  async function getPlaylist(video){
+  async function getPlaylist(video) {
     let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${video.id.videoId}&type=video&key=${API_KEY}`);
     console.log(response);
     setPlaylist(response.data);
   }
 
-  async function universalSearch(searchTerm){
+  async function universalSearch(searchTerm) {
     let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchTerm}&key=${API_KEY}`);
     setSearchResults(response.data);
     setVideo(response.data.items[0]);
   }
 
-  function getVideo(video){
+  function getVideo(video) {
     setVideo(video);
     getPlaylist(video)
 
-   }
+  }
 
-   if(video !== undefined && playlist !== undefined && searchResults !== undefined){
-     return (
-       <div>
-         <div className='container'>
-           <div className='row' style={{'marginTop':'2em','textAlign':'center'}}>
-             <SearchBar universalSearch={universalSearch}/>
-           </div>
-   
-           <div className='row' style={{'marginTop':'1em','textAlign':'center'}}>
-               <VideoPlayer video={video}/>
-               <RelatedVideos playlist={playlist.items} getVideo={getVideo}/>
-           </div>
-   
-           <div className='row'>
-               <SearchResults searchResults={searchResults.items} getVideo={getVideo}/>
-           </div>
-   
-         </div>
-       </div>
-     )
-   }
+  if (video !== undefined && playlist !== undefined && searchResults !== undefined) {
+    return (
+      <div>
+        <div className='container'>
+          <div className='row' style={{ 'marginTop': '2em', 'textAlign': 'center' }}>
+            <SearchBar universalSearch={universalSearch} />
+          </div>
 
-   else {
-     return (
-      <div className="spinner-border text-secondary position-absolute top-50 start-50" role="status">
-      <span className="visually-hidden">Loading...</span>
+          <div className='row' style={{ 'marginTop': '1em', 'textAlign': 'center' }}>
+            <VideoPlayer video={video} />
+            <RelatedVideos playlist={playlist.items} getVideo={getVideo} />
+          </div>
+
+          <div className='row'>
+            <SearchResults searchResults={searchResults.items} getVideo={getVideo} />
+          </div>
+
+        </div>
       </div>
-     )
-   }
-   
+    )
+  }
+
+  else {
+    return (
+      <div className="spinner-border text-secondary position-absolute top-50 start-50" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    )
+  }
+
 }
 
 export default App;
