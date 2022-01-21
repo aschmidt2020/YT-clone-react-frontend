16420da4a51c9a007923388f9109ccee7e2719c7
Audrey Schmidt <audsch@outlook.com>
2022-01-21 11:41:48 -0600

    fixed dot notation for getting user information in Comment List

--- src/Components/CommentList/CommentList.jsx
+++ src/Components/CommentList/CommentList.jsx
@@ -19,11 +19,11 @@ const CommentList = (props) => {
                 <tr key={comment.id}>
                     <td>{comment.user.username}</td>
                     <td>{comment.text}</td>
-                    <td>{props.user === comment.user &&
+                    <td>{props.user.user_id === comment.user.id &&
                     <CommentUpdate comment={comment} updateComment={props.updateComment}/>
                     }
                     </td>
-                <td>{props.user === comment.user && 
+                <td>{props.user.user_id === comment.user.id && 
                  <button className="btn btn-outline-secondary" onClick={() => props.deleteComment(comment)}
                  data-toggle='popover' title='Delete Comment' data-content='Delete Comment' trigger='hover'><i className="bi bi-trash"></i></button>}
                    </td>
