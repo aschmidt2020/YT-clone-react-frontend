7e0e2e1d75d3481eec39b822b84b6257a61dfc5f
Audrey Schmidt <audsch@outlook.com>
2022-01-25 10:16:05 -0600

    changed navbar to size navbar-expand-md

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ import logo from "../Images/YouTubeCloneLogo.png"
 const NavBar = (props) => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+            <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top">
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
