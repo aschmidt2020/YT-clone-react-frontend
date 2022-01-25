b3fde5463c169231929e773b0d9fd35625466d40
Audrey Schmidt <audsch@outlook.com>
2022-01-25 13:28:12 -0600

    refactored logic for displaying like/dislike button component

--- src/Components/LikeDislikeButton/LikeDislikeButton.jsx
+++ src/Components/LikeDislikeButton/LikeDislikeButton.jsx
@@ -34,60 +34,19 @@ const LikeDislikeButton = (props) => {
         props.updateComment(updatedComment);
     }
 
-    if (props.comment.likes > 0 && props.comment.dislikes > 0) {
-        return (
-            <span>
-                <Button variant="btn btn-outline-secondary" onClick={handleClick} style={{ "marginTop": "1em" }} data-toggle="popover" title="Like" data-content="Like" trigger="hover">
-                    <i className="bi bi-hand-thumbs-up-fill"></i> {props.comment.likes}
-                </Button>
-
-                <Button variant="btn btn-outline-secondary" onClick={handleClickDislike} style={{ "marginTop": "1em", "marginLeft": "1em", "marginRight": "1em" }} data-toggle="popover" title="Dislike" data-content="Dislike" trigger="hover">
-                    <i className="bi bi-hand-thumbs-down-fill"></i> {props.comment.dislikes}
-                </Button>
-            </span>
-        )
-
-    }
-
-    else if (props.comment.likes > 0 && props.comment.dislikes === 0) {
-        return (
-            <span>
-                <Button variant="btn btn-outline-secondary" onClick={handleClick} style={{ "marginTop": "1em" }} data-toggle="popover" title="Like" data-content="Like" trigger="hover">
-                    <i className="bi bi-hand-thumbs-up-fill"></i> {props.comment.likes}
-                </Button>
-
-                <Button variant="btn btn-outline-secondary" onClick={handleClickDislike} style={{ "marginTop": "1em", "marginLeft": "1em", "marginRight": "1em" }} data-toggle="popover" title="Dislike" data-content="Dislike" trigger="hover">
-                    &nbsp;<i className="bi bi-hand-thumbs-down"></i>&nbsp;&nbsp;
-                </Button>
-            </span>
-        )
-    }
-
-    else if (props.comment.likes === 0 && props.comment.dislikes > 0) {
-        return (
-            <span>
-                <Button variant="btn btn-outline-secondary" onClick={handleClick} style={{ "marginTop": "1em" }} data-toggle="popover" title="Like" data-content="Like" trigger="hover">
-                    &nbsp;<i className="bi bi-hand-thumbs-up"></i>&nbsp;&nbsp;
-                </Button>
-
-                <Button variant="btn btn-outline-secondary" onClick={handleClickDislike} style={{ "marginTop": "1em", "marginLeft": "1em", "marginRight": "1em" }} data-toggle="popover" title="Dislike" data-content="Dislike" trigger="hover">
-                    <i className="bi bi-hand-thumbs-down-fill"></i> {props.comment.dislikes}
-                </Button>
-            </span>
-        )
-    }
     return (
         <span>
             <Button variant="btn btn-outline-secondary" onClick={handleClick} style={{ "marginTop": "1em" }} data-toggle="popover" title="Like" data-content="Like" trigger="hover">
-                &nbsp;<i className="bi bi-hand-thumbs-up"></i>&nbsp;&nbsp;
+                {props.comment.likes > 0 && <span><i className="bi bi-hand-thumbs-up-fill"></i>{props.comment.likes}</span>}
+                {props.comment.likes === 0 && <span>&nbsp;<i className="bi bi-hand-thumbs-up"></i>&nbsp;&nbsp;</span>}
             </Button>
 
             <Button variant="btn btn-outline-secondary" onClick={handleClickDislike} style={{ "marginTop": "1em", "marginLeft": "1em", "marginRight": "1em" }} data-toggle="popover" title="Dislike" data-content="Dislike" trigger="hover">
-                &nbsp;<i className="bi bi-hand-thumbs-down"></i>&nbsp;&nbsp;
+                {props.comment.dislikes > 0 && <span><i className="bi bi-hand-thumbs-down-fill"></i> {props.comment.dislikes}</span>}
+                {props.comment.dislikes === 0 && <span>&nbsp;<i className="bi bi-hand-thumbs-down"></i>&nbsp;&nbsp;</span>}
             </Button>
         </span>
-
-    );
+    )
 }
 
 export default LikeDislikeButton;
